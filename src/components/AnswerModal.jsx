be0cbe6f8c1393e4028fbfa97e32360c1c610a5a
ThomasNine <thomasnine48@gmail.com>
2024-03-12 21:45:32 +0630

    for demo deploy in vercel

--- src/components/AnswerModal.jsx
+++ src/components/AnswerModal.jsx
@@ -1,4 +1,3 @@
-import { useGetAnswersQuery } from "../store/api_services/endpoints/answers.endpoint";
 import { Button } from "./ui/button";
 import {
   Dialog,
@@ -9,47 +8,39 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
+import { answers } from "../db/mock";
 
 export function AnswerModal({ answer, questionNo }) {
-  const { data, isLoading, isError, isSuccess } = useGetAnswersQuery();
+  const data = answers;
+  let filterData = data.filter(
+    (i) => i.questionNo === questionNo && i.answerNo === answer.engValue
+  );
 
-  if (isLoading) {
-    return "isloading";
-  }
-  if (isError) {
-    return "error fetching";
-  }
-  if (isSuccess) {
-    let filterData = data.filter(
-      (i) => i.questionNo === questionNo && i.answerNo === answer.engValue
-    );
-
-    return (
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline" className=" w-12">
-            {answer.mmValue}
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-md">
-          <DialogHeader>
-            <DialogTitle className=" leading-7 text-center mt-3 mb-1">
-              {filterData[0]?.answerResult}
-            </DialogTitle>
-          </DialogHeader>
-          <DialogFooter className="">
-            <DialogClose asChild>
-              <Button
-                type="button"
-                variant="secondary"
-                className="mx-auto w-full"
-              >
-                ပိတ်ပါ
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    );
-  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className=" w-12">
+          {answer.mmValue}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle className=" leading-7 text-center mt-3 mb-1">
+            {filterData[0]?.answerResult}
+          </DialogTitle>
+        </DialogHeader>
+        <DialogFooter className="">
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant="secondary"
+              className="mx-auto w-full"
+            >
+              ပိတ်ပါ
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,59 +1,41 @@
 import React, { useState } from "react";
-import { useGetQuestionsQuery } from "../store/api_services/endpoints/questions.endpoint";
 import { motion } from "framer-motion";
 import DataList from "../components/DataList";
 import GoToTopBtn from "../components/GoToTopBtn";
 import { Button } from "../components/ui/button";
-import { DataListLoading } from "../components/DataListLoading";
-
+import { questions } from "../db/mock.js";
 const HomePage = () => {
   const [endData, setEndData] = useState(15);
 
-  const {
-    data: questions,
-    isError,
-    isLoading,
-    isSuccess,
-  } = useGetQuestionsQuery();
-
-  if (isLoading) {
-    return <DataListLoading />;
-  }
-  if (isError) {
-    return "error";
-  }
-
-  if (isSuccess) {
-    const handleMoreBtn = () => {
-      setEndData(endData + 15);
-    };
-    const startData = 0;
-    const endDataToShow = Math.min(endData, questions.length);
-    const currentData = questions.slice(startData, endDataToShow);
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 1 } }}
-        exit={{ opacity: 0 }}
-      >
-        <div className=" py-5">
-          <DataList data={currentData} />
-          {currentData.length !== questions.length && (
-            <Button
-              variant="outline"
-              className=" w-full py-5 mt-5 rounded-full"
-              onClick={handleMoreBtn}
-            >
-              More
-            </Button>
-          )}
-        </div>
-        <div className="fixed bottom-5 right-3">
-          <GoToTopBtn />
-        </div>
-      </motion.div>
-    );
-  }
+  const handleMoreBtn = () => {
+    setEndData(endData + 15);
+  };
+  const startData = 0;
+  const endDataToShow = Math.min(endData, questions.length);
+  const currentData = questions.slice(startData, endDataToShow);
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 1 } }}
+      exit={{ opacity: 0 }}
+    >
+      <div className=" py-5">
+        <DataList data={currentData} />
+        {currentData.length !== questions.length && (
+          <Button
+            variant="outline"
+            className=" w-full py-5 mt-5 rounded-full"
+            onClick={handleMoreBtn}
+          >
+            More
+          </Button>
+        )}
+      </div>
+      <div className="fixed bottom-5 right-3">
+        <GoToTopBtn />
+      </div>
+    </motion.div>
+  );
 };
 
 export default HomePage;
